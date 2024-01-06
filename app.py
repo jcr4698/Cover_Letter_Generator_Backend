4c8c89d3f5ff92de3f23cfcbb7f4c0de9cf7469d
Jan <janrubio@utexas.edu>
2024-01-05 18:17:30 -0600

    added flask api access to generator

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, send_file, request, jsonify
+import json
+
+from cover_letter import CoverLetter
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+	return "Hello, World!"
+
+@app.route("/cover_letter", methods=["GET", "POST"])
+def generate_cover_letter():
+
+	try:
+		cover_letter_info = request.get_json()
+		if(cover_letter_info):
+			print(cover_letter_info)
+
+			curr_cov_let = CoverLetter()
+			curr_cov_let.set_full_name(cover_letter_info["full_name"])
+			curr_cov_let.set_phone_num(cover_letter_info["phone_num"])
+			curr_cov_let.set_email(cover_letter_info["email"])
+			curr_cov_let.set_linkedin(cover_letter_info["linkedin"])
+			curr_cov_let.set_company(cover_letter_info["company"])
+			curr_cov_let.set_cover_letter_body(cover_letter_info["cover_letter_body"])
+
+			curr_cov_let.generate_cover_letter("example.pdf")
+			return send_file("./pdfs/example.pdf")
+
+	except Exception:
+		print("nothing")
+		return "Nothing to show"
